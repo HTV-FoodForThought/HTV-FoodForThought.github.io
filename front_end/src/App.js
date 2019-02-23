ff913de2557544f06b69654ae1f7c876a758b314
ThisIsAFakeName <brianchim1@gmail.com>
2019-02-23 07:07:13 -0500

    Add keyword exact to get routes on new page

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
 import Search from './Search.js';
-import title from './resources/title-image.jpg';
+import Title from './Title.js';
 
 class App extends Component {
   render() {
@@ -10,11 +10,9 @@ class App extends Component {
       <Router>
         <div className="App">
          <header className="App-header">
-           <h1>Food for Thought</h1>
-           {/*image from https://nypost.com/2018/04/04/youre-likely-eating-tiny-chunks-of-plastic-in-home-cooked-meals/*/}
-           <img src={title} alt="Tasty food being cooked."/>
-           <Link to="/search">Begin</Link>
-           <Route path="/search" component={Search}/>
+           <Route exact path="/" component={Title}/>
+           <Route exact path="/search" component={Search}/>
+           {this.props.children}
           </header>
         </div>
       </Router>
