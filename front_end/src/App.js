820afc73c35ce882977dd57536bd9795db80e833
ThisIsAFakeName <brianchim1@gmail.com>
2019-02-23 06:32:04 -0500

    Attempt at routing

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 import './App.css';
+import Search from './Search.js';
 import title from './resources/title-image.jpg';
 
 class App extends Component {
@@ -10,9 +12,7 @@ class App extends Component {
           <h1>Food for Thought</h1>
           {/*image from https://nypost.com/2018/04/04/youre-likely-eating-tiny-chunks-of-plastic-in-home-cooked-meals/*/}
           <img src={title} alt="Tasty food being cooked."/>
-          <button onClick={() => this.handleClick()}> 
-            Begin
-          </button>
+          <Link to="/search">Begin</Link>
         </header>
       </div>
     );
---
+++ front_end/src/Search.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './Search.css';
+
+class Search extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            loadingImage: false,
+            selectedImage: false,
+            selectedIngredient: false,
+        };
+    }
+
+    render() {
+        return(
+            <div className="search">
+                <h1>Add an Ingredient</h1>
+                <input type="file"/>
+                <button onClick={this.handleClick()}>
+                    Browse
+                </button>
+            </div>
+        );
+    }
+}
+export default Search
