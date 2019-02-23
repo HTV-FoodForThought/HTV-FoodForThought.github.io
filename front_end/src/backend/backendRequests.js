c0cd1f358f4a00fc125819c22fe0a7a5c3d6c4bd
datrueonejay <jayarquelada@gmail.com>
2019-02-23 05:15:27 -0500

    Added back end requests

---
+++ front_end/src/backend/backendRequests.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const base_url = 'http://loclahost:5000/'
+
+export function upload_photo(image) {
+    return axios({
+        method: 'post',
+        url: `${base_url}upload_photo`,
+        data: {
+            image: image
+        }
+    });
+}
+
+// ingredients should be an array of strings
+export function get_recipes(ingredients) {
+    let url = `${base_url}get_recipes?ingredients=`;
+    ingredients.forEach(element => {
+        url += element + ','
+    });
+    url = ingredients.substring(0, url.length - 1);
+    return axios({
+        method: 'get',
+        url: url
+    });
+}
