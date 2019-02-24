e4fc326d89509b61123c149641d1601f38e37579
Jayden <jayarquelada@gmail.com>
2019-02-24 09:37:01 -0500

    Implemented camera feature

--- front_end/src/Search.js
+++ front_end/src/Search.js
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom';
 import placeholder from './resources/placeholder.jpg'
 import LoadingOverlay from 'react-loading-overlay';
 import { upload_photo } from './backend/backendRequests.js';
-import { ListItem, Grid, IconButton, Chip } from '@material-ui/core';
+import { ListItem, Grid, IconButton, Chip, Button } from '@material-ui/core';
+import Webcam from 'react-webcam';
 
 class Search extends Component {
     constructor(props) {
@@ -17,21 +18,38 @@ class Search extends Component {
             currImageIngredients: [],
             selectedIngredients: [],
             ingredientList: [],
+            takePhoto: false,
+            selfieCam: true,
         };
     }
 
+    setRef = webcam => {
+        this.webcam = webcam;
+    }
+
     render() {
+        console.log(this.state.selfieCam);
+        const videoSettings = this.state.selfieCam ? {
+            facingMode: "user"
+        } : {
+            facingMode: { exact: "environment" }
+        };
         return(
             <Grid container>
                 <Grid item xs={12}><h1>Add an Ingredient</h1></Grid>
                 <Grid container item xs={12} lg={8}>
                     {/*https://countrylakesdental.com/wp-content/uploads/2016/10/orionthemes-placeholder-image.jpg*/}
-                    <Grid item xs={12}><img src= {this.state.selectedImage} alt="Uploaded Ingredient" className="ingredientImage"/></Grid>                    
-                    <Grid item xs={12}><input type="file" onChange ={(e) => this.handleFile(e)} className = "selectFile"/></Grid> 
+                    <Grid item xs={12}>{this.state.takePhoto ? <Webcam videoConstraints={videoSettings} audio={false} ref={this.setRef} screenshotFormat='image/jpeg'/> : <img src= {this.state.selectedImage} alt="Uploaded Ingredient" className="ingredientImage"/>}</Grid>                    
+                    <Grid item xs={12} m={3}><input type="file" onChange ={(e) => this.handleFile(e)} className = "selectFile"/></Grid>
+                    <Grid item xs={12} m={3}><Button variant='contained' onClick={()=>this.setState({...this.state, takePhoto: !this.state.takePhoto})}>{this.state.takePhoto ? "Stop taking photo" : "Take Photo Instead"}</Button></Grid> 
+                    {this.state.takePhoto ? (<Grid container xs={12}><Grid item xs={12} m={3}><Button variant='contained' onClick={()=>{this.takePhoto()}}>Capture Photo</Button></Grid>
+                                            <Grid item xs={12} m={3}><Button variant='contained' onClick={()=>{this.setState({...this.state, selfieCam: !this.state.selfieCam})}}>Switch Camera</Button></Grid></Grid>)
+                                            : null 
+                    }
                 </Grid> 
 
                 {/*title + upload + placeholder image*/}
-                <Grid container item xs={12} lg={4} >
+                <Grid container item xs={12} lg={4} >   
                     <Grid item xs={12}><p>Select Matching Ingredient:</p></Grid>
                     {this.state.loadingImage ? 
                         <LoadingOverlay active={this.state.loadingImage} spinner text='Loading your content...' className='centerInPage'/>
@@ -56,6 +74,8 @@ class Search extends Component {
         e.preventDefault();
         let r = new FileReader();
         let file = e.target.files[0];
+        console.log(file.type);
+        console.log(file);
         this.setState({...this.state, firstLoad: false, loadingImage: true});
         r.onloadend = () => {
             this.setState({...this.state, selectedImage: r.result, imageFile: file});
@@ -77,6 +97,89 @@ class Search extends Component {
             console.log(error);
         });
     }
+
+    takePhoto = () => {
+        let url = this.webcam.getScreenshot();
+        let r = new FileReader();
+        r.onloadend = () => {
+            this.setState({...this.state, selectedImage: r.result});
+        }
+
+        fetch(url)
+        .then(res => res.blob())
+        .then(
+            blob =>  {
+                r.readAsDataURL(blob);
+
+                this.setState({...this.state, takePhoto: false, imageFile: blob});
+                upload_photo(blob).then((responseJson) => {
+                    let copy = [];
+                    responseJson.data.response.forEach(element => {
+                        copy.push(
+                            <Grid item xs={6} sm={4} m={3} lg={6}>
+                                <ListItem button onClick={this.addIngredient.bind(this,element)}>
+                                    {element}
+                                </ListItem>
+                            </Grid>
+                        )
+                    });
+                    this.setState({...this.state, currImageIngredients: copy, loadingImage: false});
+                }) .catch((error) => {
+                    console.log(error);
+                });
+            }
+        )
+        // let r = new FileReader();
+        // r.onloadend = () => {
+        //     this.setState({...this.state, selectedImage: r.result, imageFile: pic});
+        // }
+        // console.log(pic.type)
+        // console.log(pic);
+        // r.readAsDataURL(pic);
+        // this.setState({...this.state, takePhoto: false, selectedImage: pic, imageFile: pic});
+
+        // upload_photo(pic).then((responseJson) => {
+        //     let copy = [];
+        //     responseJson.data.response.forEach(element => {
+        //         copy.push(
+        //             <Grid item xs={6} sm={4} m={3} lg={6}>
+        //                 <ListItem button onClick={this.addIngredient.bind(this,element)}>
+        //                     {element}
+        //                 </ListItem>
+        //             </Grid>
+        //         )
+        //     });
+        //     this.setState({...this.state, currImageIngredients: copy, loadingImage: false});
+        // }) .catch((error) => {
+        //     console.log(error);
+        // });
+    }
+
+    //https://stackoverflow.com/questions/16245767/creating-a-blob-from-a-base64-string-in-javascript
+    b64toBlob = (b64Data, contentType, sliceSize) => {
+        contentType = contentType || '';
+        sliceSize = sliceSize || 512;
+      
+        var byteCharacters = atob(b64Data);
+        var byteArrays = [];
+      
+        for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+          var slice = byteCharacters.slice(offset, offset + sliceSize);
+      
+          var byteNumbers = new Array(slice.length);
+          for (var i = 0; i < slice.length; i++) {
+            byteNumbers[i] = slice.charCodeAt(i);
+          }
+      
+          var byteArray = new Uint8Array(byteNumbers);
+      
+          byteArrays.push(byteArray);
+        }
+      
+        var blob = new Blob(byteArrays, {type: contentType});
+        return blob;
+    };
+
 
     addIngredient(element) {
         if (!this.state.selectedIngredients.includes(element)) {
