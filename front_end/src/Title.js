57f7b8f2a532d63ed68e7f9b66f9b40a9c77ad4d
patrick-aoc <patrick.ocampo@mail.utoronto.ca>
2019-02-23 23:29:32 -0500

    Added a few pics

--- front_end/src/Title.js
+++ front_end/src/Title.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './Title.css';
 import title from './resources/title-image.jpg';
+import yeet from './resources/thot.png';
 
 class Title extends Component {
     render() {
         return (
             <div className="title">
                 <div className="title-header">
+                    <img src={yeet} alt="Thought bubble." className="thot-image"/>
                     <h1>Food for Thought</h1>
                     {/*image from https://nypost.com/2018/04/04/youre-likely-eating-tiny-chunks-of-plastic-in-home-cooked-meals/*/}
                     <img src={title} alt="Tasty food being cooked." className="title-image"/>
