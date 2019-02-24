1bf41465a2ba35f8fec7f56b0d305fe2da954a80
patrick-aoc <patrick.ocampo@mail.utoronto.ca>
2019-02-23 22:03:35 -0500

    Began work on CSS

--- front_end/src/Title.js
+++ front_end/src/Title.js
@@ -11,7 +11,8 @@ class Title extends Component {
                     <h1>Food for Thought</h1>
                     {/*image from https://nypost.com/2018/04/04/youre-likely-eating-tiny-chunks-of-plastic-in-home-cooked-meals/*/}
                     <img src={title} alt="Tasty food being cooked." className="title-image"/>
-                <Link to="/search">Begin</Link>
+                    <h4>You have the ingredients, we got the dishes</h4>
+                    <Link className="title-btn" to="/search">Begin</Link>
                 </div>
             </div>
         );
